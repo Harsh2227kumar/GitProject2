76bb9919fb11ed1c175762c8f44394b18c61bd12
Harsh2227kumar <harsh2227kumar@gmail.com>
2022-01-12 19:39:12 +0530

    Till Mobile number Xml

--- app/src/main/java/com/harsh/chatsgo/PhoneNumberActivity.java
+++ app/src/main/java/com/harsh/chatsgo/PhoneNumberActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class PhoneNumberActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_phone_number);
     }
 }
